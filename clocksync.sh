11114e4f7d3855117346dd36371c1599e828ec3d
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2023-10-25 08:20:04 -0400

    Update clocksync.sh
    Error in if-elif-else logic and added output to know what the script is doing.

--- clocksync.sh
+++ clocksync.sh
@@ -2,12 +2,14 @@
 
 if [ $# -gt 1 ]; then
   echo "Usage: $0 <domain : null to UNSYNC>"
-elif [ $# == 1 ]
+elif [ $# == 1 ]; then
   # Sync time to domain
+  echo "[*] Syncing Clock with $1"
   sudo timedatectl set-ntp 0
   sudo ntpdate -u $1 && date
 else # No Domain Provided
   # Unsync clock
+  echo "[*] Unsyncing Clock"
   sudo ntpdate 0.debian.pool.ntp.org
   sudo hwclock --systohc
   sudo systemctl enable --now systemd-timesyncd
