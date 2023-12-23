ad69a1a73884f1556f16e3af41c8d83c69578826
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2023-12-23 17:29:29 -0500

    Update clocksync.sh
    check for ntpdate and edits for first time clock sync'rs

--- clocksync.sh
+++ clocksync.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+exists()
+{
+  command -v "ntpdate" >/dev/null 2>&1
+}
+
+if [ ! exists ]; then
+  echo "[-] ntpdate not installed on your machine!"
+  sudo apt install ntpdate
+fi
+
+# Unsync clock
+echo "[*] Unsyncing Clock"
+sudo ntpdate 0.debian.pool.ntp.org
+sudo hwclock --systohc
+sudo systemctl enable --now systemd-timesyncd
+sudo timedatectl set-ntp true
+
 if [ $# -gt 1 ]; then
   echo "Usage: $0 <domain : null to UNSYNC>"
 elif [ $# == 1 ]; then
   # Sync time to domain
   echo "[*] Syncing Clock with $1"
   sudo timedatectl set-ntp 0
-  sudo ntpdate -u $1 && date
-else # No Domain Provided
-  # Unsync clock
-  echo "[*] Unsyncing Clock"
-  sudo ntpdate 0.debian.pool.ntp.org
-  sudo hwclock --systohc
-  sudo systemctl enable --now systemd-timesyncd
-  sudo timedatectl set-ntp true
+  sudo ntpdate $1
 fi
