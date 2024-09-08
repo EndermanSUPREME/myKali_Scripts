a6973db5969cc88dfb4df9f52c70ae5aba8115d3
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2024-09-08 13:05:19 -0400

    Update BuildEnder.sh
    Modified my build file for automation using apt's -y

--- BuildEnder.sh
+++ BuildEnder.sh
@@ -5,47 +5,26 @@ if [ "$(id -u)" -ne 0 ]; then
         echo "Please run as root." >&2; exit 1;
 fi
 
-# run an input test so if the user wishes for pure automation
-# they can have it set up
-echo "Did you run '#0 < yes'?"
-read yesTest
-
-if [ yesTest != "y" ]; then
-        exit 0;
-fi
-
 # Fresh Install Update
 apt update
 
 # Install Tools, Binaries, and Wordlists
+apt install -y python3-impacket impacket-scripts crackmapexec gobuster nodejs ntpdate
 
-apt install python3-impacket
-apt install impacket-scripts
-apt install crackmapexec
-apt install gobuster
-apt install nodejs
-
-apt install ntpdate
 # Installs hwclock
-apt-get install util-linux
-apt install util-linux-extra
+apt-get install -y util-linux
+apt install -y util-linux-extra
 
-apt install seclists
+apt install -y seclists
 mv /usr/share/seclists/ /usr/share/wordlists/Seclists
 
-apt install neo4j
-apt update && apt install -y bloodhound
+apt install -y neo4j bloodhound ncurses-hexedit
 pip install bloodhound impacket ldap3 dnspython
 
-apt install ncurses-hexedit
+# Make sure msf console & vemon are updated and ready
+apt install -y metasploit-framework ghidra gdb jq
 
-// make sure msf console & vemon are updated and ready
-apt install metasploit-framework
-
-apt install ghidra
-apt install gdb
-
-apt install jq
+/usr/bin/vim +PlugInstall +qall
 
 # Install Obsidian
 wget 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/Obsidian-1.5.3.AppImage' -O Obsidian;
@@ -53,16 +32,10 @@ chmod +x Obsidian;
 mv ./Obsidian -t /usr/local/bin
 
 # Install my local bins
-wget https://raw.githubusercontent.com/EndermanSUPREME/myKali_Scripts/main/setup.py -O buildSpawnTerminal.py
-
 wget https://raw.githubusercontent.com/EndermanSUPREME/myKali_Scripts/main/clocksync.sh; chmod +x clocksync.sh; mv ./clocksync.sh /usr/local/bin/clocksync
 
 # Final Updates / Upgrades
-apt update; apt full-upgrade -y
+apt update
+apt full-upgrade -y
 
-# Final Notes
-clear
-echo '[*] python3 buildSpawnTerminal.py : will need to be Manually ran!'
-echo 'To allow Impacket to work properly, ensure you add this line to ~/.bashrc:'
-echo '----> export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages'
 echo '[+] VM Established!'
