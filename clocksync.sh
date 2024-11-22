865b494efcd5167a7458629358a58a088567f4ea
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2024-11-21 22:25:16 -0500

    Update clocksync.sh
    Added a note about clock syncing for Oracle VM users

--- clocksync.sh
+++ clocksync.sh
@@ -17,6 +17,11 @@ fi
 # Perform a setup to disable time sync?
 sudo timedatectl set-ntp 0
 
+# rdate and ntpdate commands work
+# if you have clock skew issues against windows targets
+# if you are using Oracle VM you will need to run this
+# command on the Host machine.
+# (Windows) cmd.exe | .\VBoxManage.exe setextradata "VM Name" "VBoxInternal/Devices/VMMDev/0/Config/GetHostTimeDisabled"
 if [ $# -gt 1 ]; then
   echo "Usage: $0 <domain : null to UNSYNC>"
 elif [ $# == 1 ]; then
